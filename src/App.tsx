7978599def129a7550fc1fcade5a41075a0ce4c1
mashijifu <4ts0t0i7c@gmail.com>
2024-07-14 21:37:00 +0900

    ハローコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import Hello from './components/Hello';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Reactコース</h1>
       <Button />
+      <Hello />
     </div>
   );
 }
---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hello: React.FC = () => {
+
+  return (
+    <div>
+      <h1>Helloコンポーネント</h1>
+      <p>Hello,World</p>
+    </div>
+  );
+};
+
+export default Hello;
