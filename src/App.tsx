a0eb0dcdbd92d1f3ee96df3e0f780e823a79e2ac
mashijifu <4ts0t0i7c@gmail.com>
2024-07-20 16:52:24 +0900

    フォーム課題2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import { useState } from 'react';
 
+interface Courses {
+  id: number;
+  name: string;
+}
+
 const App: React.FC = () => {
   const [name, setName] = useState("");
+  const [checkedCourse, setCheckedCourse] = useState(1);
 
-  const changeName = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const courses: Courses[] = [
+    { id: 1, name: "Vueコース"},
+    { id: 2, name: "Angularコース"},
+    { id: 3, name: "Reactコース"},
+  ];
+
+  const handleChangeName = (e: any) => {
     setName(e.target.value);
+  };
+
+  const handleChangeCourse = (e: any) => {
+    setCheckedCourse(Number(e.target.value));
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log(name);
-  }
+    console.log(checkedCourse);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -20,8 +37,28 @@ const App: React.FC = () => {
         <input
           type="text"
           value={name}
-          onChange={changeName}
+          onChange={handleChangeName} 
         />
+      </div>
+      <div>
+        <label>コース: </label>
+        {
+          courses.map((course) => {
+            return (
+              <label htmlFor={course.id.toString()} key={course.id}>
+                <input
+                  id={course.id.toString()}
+                  type="radio"
+                  name="course"
+                  value={course.id}
+                  checked={checkedCourse === course.id}
+                  onChange={handleChangeCourse}
+                />
+                {course.name}
+              </label>
+            );
+          })
+        }
       </div>
       <div>
         <input type="submit" value="送信" />
