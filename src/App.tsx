eda7b5a021c4ac84ebb46e7e84ce4071d3240868
mashijifu <4ts0t0i7c@gmail.com>
2024-07-20 15:54:42 +0900

    条件分岐課題2

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 const App: React.FC = () => {
   const num: number = 10;
 
-  if(num >= 10) {
-    return <h1>TypeScript</h1>;
-  } else {
-    return <h1>JavaScript</h1>;
-  }
+  return (
+    <h1>
+      {(() => {
+        if(num >= 10) {
+          return <h1>TypeScript</h1>
+        } else {
+          return <h1>JavaScript</h1>
+        }
+      })()}
+    </h1>
+  )
 };
 
 export default App;
