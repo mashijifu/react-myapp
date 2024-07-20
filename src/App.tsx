a27261fbe653633052c34ac581a51bc2dade021f
mashijifu <4ts0t0i7c@gmail.com>
2024-07-20 17:11:45 +0900

    react-router導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,21 @@
 import React from 'react';
-import { useState } from 'react';
-
-interface Courses {
-  id: number;
-  name: string;
-}
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App: React.FC = () => {
-  const [name, setName] = useState("");
-  const [checkedCourse, setCheckedCourse] = useState(1);
-
-  const courses: Courses[] = [
-    { id: 1, name: "Vueコース"},
-    { id: 2, name: "Angularコース"},
-    { id: 3, name: "Reactコース"},
-  ];
-
-  const handleChangeName = (e: any) => {
-    setName(e.target.value);
-  };
-
-  const handleChangeCourse = (e: any) => {
-    setCheckedCourse(Number(e.target.value));
-  }
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(name);
-    console.log(checkedCourse);
-  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>氏名: </label>
-        <input
-          type="text"
-          value={name}
-          onChange={handleChangeName} 
-        />
-      </div>
-      <div>
-        <label>コース: </label>
-        {
-          courses.map((course) => {
-            return (
-              <label htmlFor={course.id.toString()} key={course.id}>
-                <input
-                  id={course.id.toString()}
-                  type="radio"
-                  name="course"
-                  value={course.id}
-                  checked={checkedCourse === course.id}
-                  onChange={handleChangeCourse}
-                />
-                {course.name}
-              </label>
-            );
-          })
-        }
-      </div>
-      <div>
-        <input type="submit" value="送信" />
-      </div>
-    </form>
+    <div className="App">
+      <h1>Reactコース</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 };
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+
+  return (
+    <div>
+      <h2>Aboutコンポーネント</h2>
+    </div>
+  );
+};
+
+export default About;
