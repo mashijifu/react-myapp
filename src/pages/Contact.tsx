ef77ec0195b7fa3f0089bffe3f53ccbf7efe724e
mashijifu <4ts0t0i7c@gmail.com>
2024-07-20 17:20:43 +0900

    useNavigateでページ移動

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Contact: React.FC = () => {
+  const navigate = useNavigate();
+  const onClickButton = (): void => {
+    navigate('/');
+  }
 
   return (
     <div>
       <h2>Contactコンポーネント</h2>
+      <button onClick={onClickButton}>Homeへ</button>
     </div>
   );
 };
