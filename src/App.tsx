9caff09fd0b5ecef188c6be246207c147c4abdea
mashijifu <4ts0t0i7c@gmail.com>
2024-07-20 17:22:28 +0900

    404ページ実装

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => {
 
@@ -34,6 +35,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+
+  return (
+    <div>
+      <h2>このページは存在しません</h2>
+    </div>
+  );
+};
+
+export default NotFound;
