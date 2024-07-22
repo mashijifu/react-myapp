d72535fac3c261d441d647fd7ff913cd9eb7bd55
mashijifu <4ts0t0i7c@gmail.com>
2024-07-22 09:38:36 +0900

    useMemo課題1

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
-import './App.css';
-import { useState, useMemo } from 'react';
-import useCounter from './hooks/useCounter';
-
+import { useState, useMemo } from "react";
 
 const App: React.FC = () => {
-  const counter1 = useCounter(0);
-  const counter2 = useCounter(100);
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
 
-  const double = (count: number): number =>  {
+  const heavyProcess = (count: number): number =>  {
     let i = 0;
     while (i < 1000000000) i++;
-    return count * 2;
+    return i + count;
   }
 
-  const doubleCount = useMemo(() => double(count1), [count1]);
+  const heavyCount = useMemo(() => heavyProcess(count2), [count2]);
 
-  // useMemoを利用しない場合の記述
-  // const doubleCount = double(count1);
+  const onChangeInputValue = (e: any) => {
+    setInputValue(Number(e.target.value));
+  }
 
   return (
-    <div className='App'>
-      <p>{counter1.count}</p>
-      <button onClick={counter1.increment}>+</button>
-      <button onClick={counter1.decrement}>-</button>
-      <p>{counter2.count}</p>
-      <button onClick={counter2.increment}>+</button>
-      <button onClick={counter2.decrement}>-</button>
-      <p>Count1:{count1}</p>
-      <p>doubleCount:{doubleCount}</p>
-      <button onClick={()=>setCount1(count1 + 1)}>加算1</button>
-      <p>Count2:{count2}</p>
-      <button onClick={()=>setCount2(count2 + 1)}>加算2</button>
+    <div>
+      <input type="number" onChange={(onChangeInputValue)}/>
+      <div>
+        <p>Count: {count1}</p>
+        <button onClick={()=>setCount1(count1 + inputValue)}>ボタン1</button>
+      </div>
+      <div>
+        <p>HeavyCount: {heavyCount}</p>
+        <button onClick={()=>setCount2(count2 + inputValue)}>ボタン2</button>
+      </div>
     </div>
   );
 };
